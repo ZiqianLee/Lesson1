3d53594227a220e8f002f5723d957cbb69ac369b
Jason Li <jason.ligca@gmail.com>
2026-06-15 18:00:50 -0400

    Initial commit

---
+++ lesson.js
@@ -0,0 +1,10 @@
+//alert("Hi World!")
+document.write("Hi World!")
+console.log("Hi World!")
+console.log(Infinity/-Infinity)
+
+
+var z,x,y
+x=10
+y=x+"y"
+document.write(y+"\n"+Infinity/-Infinity)
