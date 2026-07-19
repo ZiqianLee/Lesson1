04f7503b50cef15832532a1cc8db12393a359117
Jason Li <jason.ligca@gmail.com>
2026-07-19 00:03:24 -0400

    1111

---
+++ practices/mults3_5.js
@@ -0,0 +1,14 @@
+function solution(number) {
+  let sum = 0;
+  if (number <= 3) return sum;
+
+  for (let i = 3; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(solution(4));
+
