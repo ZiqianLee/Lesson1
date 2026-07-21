a1e1b8bbbb3c0e84f3b5764a54dc9d52631167bb
Jason Li <jason.ligca@gmail.com>
2026-07-21 00:36:07 -0400

    refactor: replace hypotenuse function logic with sum function call

--- E_Playground.js
+++ E_Playground.js
@@ -1,9 +1,1 @@
-function hypotenuse(a, b) {
-    function square(x) { return x*x; }
-    return Math.sqrt(square(a) + square(b));
-}
-hypotenuse(3, 4);
-
-// console.log(a); // ReferenceError: a is not defined
-// a = 10; 
-// console.log(a); // 输出 10，a被提升为全局变量
+sum();
