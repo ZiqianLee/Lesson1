465512841b2111f39473f0db4ec7bc35f7bc2d66
Jason Li <jason.li@pearfiction.com>
2026-08-06 14:41:21 -0400

    Update playground example

--- E_Playground.js
+++ E_Playground.js
@@ -1,16 +1,14 @@
-/**
- * 演示 JavaScript 中 for...in 和 for...of 循环的区别
- * for...in 遍历的是数组索引，for...of 遍历的是数组元素值
- */
-let data = [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  sum = 0;
-
-// 使用 for...in 遍历数组（遍历的是索引）
-for (let element in data) {
-  console.log(element);
+function buildTaco(firstFilling, secondFilling) {
+  return `Taco with ${firstFilling} and ${secondFilling}`;
 }
 
-// 使用 for...of 遍历数组（遍历的是元素值）
-for (let element of data) {
-  console.log(element);
+function makeMeatTaco(meatFilling) {
+  return function(otherFilling) {
+    return buildTaco(meatFilling, otherFilling);
+  };
 }
+
+let chickenTaco = makeMeatTaco('chicken');
+
+console.log(chickenTaco('grilled onions'));
+console.log(chickenTaco('salsa verde'));
