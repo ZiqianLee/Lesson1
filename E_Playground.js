e72ef5ece4619b6b75589a77913854fdc0e8dc24
Jason Lee <Jason.ligca@gmai.com>
2026-09-15 08:52:25 -0400

    qwdqdqwdqwd

--- E_Playground.js
+++ E_Playground.js
@@ -3,11 +3,8 @@ for(let i = 0; i < b.length; i++) {
     b[i] = i + 1;
     if (Math.random() > 0.5) {
         console.log("rand!")
-        //b.splice(i, 1); 
-        console.log("splice: "+b.splice(i, 1))
-        console.log(b.length)
-        console.log(b);
-        console.log("-----");
+        b.splice(i, 1, i + 1); 
+    
     }
 }
 console.log(b);
