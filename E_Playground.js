40eafc66b9bcd5114f91774565118212046a11fb
Jason Lee <Jason.ligca@gmai.com>
2026-09-15 08:50:42 -0400

    update playground splice exercise

--- E_Playground.js
+++ E_Playground.js
@@ -1,9 +1,13 @@
-var arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-for (let i = 0; i < arr.length; i++) {
-  arr[i] = i + 1;
-  if (Math.random() > 0.5) {
-    console.log(`Splice ${arr[i]} at index ${i}`);
-    arr.splice(i, 1, i + 1);
-  }
+var b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+for(let i = 0; i < b.length; i++) {
+    b[i] = i + 1;
+    if (Math.random() > 0.5) {
+        console.log("rand!")
+        //b.splice(i, 1); 
+        console.log("splice: "+b.splice(i, 1))
+        console.log(b.length)
+        console.log(b);
+        console.log("-----");
+    }
 }
-console.log(arr);
+console.log(b);
